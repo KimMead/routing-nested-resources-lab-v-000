24a803efa82d86a03c33528d6fb5a0bae8ee4ab2
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2019-03-25 13:50:26 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   resources :artists, only: [:show] do
   resources :songs, only: [:show, :index]
-end
+  end
+
+  resources :posts, only: [:index, :show, :new, :create, :edit, :update]
+
+  root 'songs#index'
 end
